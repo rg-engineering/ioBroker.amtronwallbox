b7e21b01b9acad3baffdbbe5287ef38dff28c2df
René <info@rg-engineering.eu>
2022-05-17 20:26:51 +0200

    initial

---
+++ README.md
@@ -0,0 +1,55 @@
+﻿![Logo](admin/pvoutputorg.png)
+# ioBroker.pvoutputorg
+
+![Number of Installations](http://iobroker.live/badges/pvoutputorg-installed.svg) ![Number of Installations](http://iobroker.live/badges/pvoutputorg-stable.svg)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.pvoutputorg.svg)](https://www.npmjs.com/package/iobroker.pvoutputorg)
+[![NPM version](http://img.shields.io/npm/v/iobroker.pvoutputorg.svg)](https://www.npmjs.com/package/iobroker.pvoutputorg)
+
+[![Known Vulnerabilities](https://snyk.io/test/github/rg-engineering/ioBroker.pvoutputorg/badge.svg)](https://snyk.io/test/github/rg-engineering/ioBroker.pvoutputorg)
+![GitHub Actions](https://github.com/rg-engineering/ioBroker.pvoutputorg/workflows/Test%20and%20Release/badge.svg)
+
+[![NPM](https://nodei.co/npm/iobroker.pvoutputorg.png?downloads=true)](https://nodei.co/npm/iobroker.pvoutputorg/)
+
+
+**This adapter uses Sentry libraries to automatically report exceptions and code errors to the developers.** 
+For more details and for information how to disable the error reporting see [Sentry-Plugin Documentation](https://github.com/ioBroker/plugin-sentry#plugin-sentry)! Sentry reporting is used starting with js-controller 3.0.
+
+
+**If you like it, please consider a donation:**
+                                                                          
+[![paypal](https://www.paypalobjects.com/en_US/DK/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=YBAZTEBT9SYC2&source=url) 
+
+The adapter connects to [PvOutput.org](https://pvoutput.org). System-ID and API-key is used to establish the connection. Both needs to be configured on admin page.
+The system, status and statistical data for all configured systems are currently being read and displayed in data points.
+
+For detailed information please have a look on [pvoutput.org help](https://pvoutput.org/help/overview.html)
+
+If you support pvoutput.org with a donation, additional features will be made available to you. At the moment these are not supported here in the adapter yet.
+
+Data upload is not yet implemented. Currently [sbfspot](https://github.com/SBFspot/SBFspot) can be used for this. Later the adapter will also support data upload. 
+
+
+## known issues
+* please create issues at [github](https://github.com/rg-engineering/ioBroker.pvoutputorg/issues) if you find bugs or whish new features
+   
+## Changelog
+
+### 1.0.0 (2022-04-29)
+* (René) first official release
+
+### 0.0.1 (2022-04-24)
+* (René) initial release
+
+## License
+Copyright (C) <2022>  <info@rg-engineering.eu>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,224 @@
+﻿/*
+ * pvoutputorg adapter für iobroker
+ *
+ * Created: 15.05.2022 18:39:28
+ *  Author: Rene
+*/
+
+"use strict";
+
+const utils = require("@iobroker/adapter-core");
+const axios = require('axios');
+
+/*
+ * docu see
+ https://github.com/orlopau/amtron
+ https://forum.iobroker.net/topic/32179/wallbox-mennekes-amtron-auslesen-adapter-aus-skript
+
+*/
+  
+
+
+let adapter;
+function startAdapter(options) {
+    options = options || {};
+    Object.assign(options, {
+        name: "amtronwallbox",
+        //#######################################
+        //
+        ready: function () {
+            try {
+                //adapter.log.debug('start');
+                main();
+            }
+            catch (e) {
+                adapter.log.error("exception catch after ready [" + e + "]");
+            }
+        },
+        //#######################################
+        //  is called when adapter shuts down
+        unload: function (callback) {
+            try {
+                adapter && adapter.log && adapter.log.info && adapter.log.info("cleaned everything up...");
+                //to do stop intervall
+                callback();
+            } catch (e) {
+                callback();
+            }
+        },
+        //#######################################
+        //
+        SIGINT: function () {
+            clearInterval(intervalID);
+            intervalID = null;
+            adapter && adapter.log && adapter.log.info && adapter.log.info("cleaned everything up...");
+            CronStop();
+        },
+        //#######################################
+        //  is called if a subscribed object changes
+        //objectChange: function (id, obj) {
+        //    adapter.log.debug("[OBJECT CHANGE] ==== " + id + " === " + JSON.stringify(obj));
+        //},
+        //#######################################
+        // is called if a subscribed state changes
+        //stateChange: function (id, state) {
+        //HandleStateChange(id, state);
+        //},
+        stateChange: async (id, state) => {
+            await HandleStateChange(id, state);
+        },
+        //#######################################
+        //
+    });
+    adapter = new utils.Adapter(options);
+
+    return adapter;
+}
+
+
+
+let intervalID;
+async function main() {
+
+    adapter.log.debug("start  ");
+
+    
+    await checkVariables();
+
+    subscribeVars();
+
+
+    let readInterval = 15;
+    if (parseInt(adapter.config.readInterval) > 0) {
+        readInterval = adapter.config.readInterval;
+    }
+    else {
+        adapter.log.warn("read interval not defined");
+    }
+    adapter.log.debug("read every  " + readInterval + " minutes");
+    intervalID = setInterval(Do, readInterval * 60 * 1000);
+    
+}
+
+async function Do() {
+
+    adapter.log.debug("starting ... " );
+
+    await ReadData();
+
+    //to do
+    //await WriteData();
+}
+
+
+async function ReadData(){
+
+    read()
+    
+}
+
+
+async function read() {
+
+    try {
+
+        /*
+        var abfrage_DevInfo = "curl -H 'Accept: application/json' http://10.0.1.28:25000/MHCP/1.0/DevInfo?DevKey=999999";
+        var abfrage_ChargeData = "curl -H 'Accept: application/json' http://10.0.1.28:25000/MHCP/1.0/ChargeData?DevKey=999999";
+        var abfrage_Statistics_Day = "curl -H 'Accept: application/json' http://10.0.1.28:25000/MHCP/1.0/Statistics/Day?DevKey=999999";
+        var abfrage_Statistics_Week = "curl -H 'Accept: application/json' http://10.0.1.28:25000/MHCP/1.0/Statistics/Week?DevKey=999999";
+        var abfrage_Statistics_Month = "curl -H 'Accept: application/json' http://10.0.1.28:25000/MHCP/1.0/Statistics/Month?DevKey=999999";
+        var abfrage_Statistics_Year = "curl -H 'Accept: application/json' http://10.0.1.28:25000/MHCP/1.0/Statistics/Year?DevKey=999999";
+        var abfrage_Statistics_Annual = "curl -H 'Accept: application/json' http://10.0.1.28:25000/MHCP/1.0/Statistics/Annual?DevKey=999999";
+        */
+
+
+        let sURL = "http://http://192.168.3.18:25000/MHCP/1.0/DevInfo?DevKey=GXvPp6Yv";
+
+
+        adapter.log.debug("URL " + sURL);
+
+        let buffer = await axios.get(sURL);
+
+        adapter.log.debug("got data status " + typeof buffer.data + " " + JSON.stringify(buffer.data));
+        
+
+    }
+    catch (e) {
+        adapter.log.error("exception in read [" + e + "]");
+    }
+}
+
+
+function toDate(sDate) {
+
+    //yyyymmdd
+    const year = sDate.slice(0, 4);
+    const month = sDate.slice(4, 6);
+    const day = sDate.slice(6, 9);
+
+    let oDate = new Date(year, month - 1, day);
+
+    return oDate.toLocaleDateString();
+}
+
+async function HandleStateChange(id, state) {
+   
+
+    if (state.ack !== true) {
+
+        adapter.log.debug("handle state change " + id);
+        const ids = id.split(".");
+
+        //just dummy
+        //if (ids[2] === "cmd") {
+        //    await do_Command();
+        //}
+        //else {
+            adapter.log.warn("unhandled state change " + id);
+        //}
+    }
+
+}
+
+function subscribeVars() {
+    //adapter.subscribeStates("cmd");
+}
+
+
+async function checkVariables() {
+    adapter.log.debug("init variables ");
+
+
+    
+    /*
+        key = system.Name + ".System.Longitude";
+        await adapter.setObjectNotExistsAsync(key, {
+            type: "state",
+            common: {
+                name: "Longitude",
+                type: "number",
+                role: "value",
+                read: true,
+                write: false,
+                unit: ""
+            }
+        });
+
+    */
+
+
+    
+    
+}
+
+
+
+
+// If started as allInOne/compact mode => return function to create instance
+if (module && module.parent) {
+    module.exports = startAdapter;
+} else {
+    // or start the instance directly
+    startAdapter();
+} 
