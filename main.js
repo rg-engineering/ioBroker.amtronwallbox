66a01290c0fe503aa3503e395f5ff10d931fcefe
René <info@rg-engineering.eu>
2022-12-16 18:54:30 +0100

    see issue #1:  parse data from result

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ let cronJobs = [];
  * docu see
  https://github.com/orlopau/amtron
  https://forum.iobroker.net/topic/32179/wallbox-mennekes-amtron-auslesen-adapter-aus-skript
+ https://github.com/lephisto/amtron
 
 
 für ChargeControl mit Rest Api
@@ -169,50 +170,19 @@ async function read_MHCP_DevInfo(system) {
                 Accept: 'application/json '
             }
         }
-        adapter.log.debug("URL " + sURL);
 
+        adapter.log.debug("URL " + sURL.replace(/DevKey=.*/, 'DevKey=******'));
+        
         let buffer = await axios.get(sURL, header);
 
         adapter.log.debug("got data status " + typeof buffer.data + " " + JSON.stringify(buffer.data));
         
+        if (buffer != null && buffer.status == 200 && buffer.data != null) {
 
-        if (buffer != null && buffer.status == 200 && buffer.data != null && typeof buffer.data === "string") {
-            /*
-            "DevName": [string],         Name of the device 
-            "LocTime": [number],         timestamp 
-            "Summer": [boolean],         Is summer time? 
-            "Tz": [number],              timezone offset in minutes 
-            "ItemNo": [string],          item number 
-            "Sn": [number],              Serial Number 
-            "Hcc3": [string],            Info about the HCC3 (main controller) 
-            "Hmi": [string],             Info about hardware software? 
-            "Rfid": [string],            Info about RFID? 
-            "Wifi": [string],            WiFi module version? 
-            "FixedVehCosts": [number],   Fixed vehicle costs as specified in the app 
-            "OldVehCosts": [number],     OldVehicle costs? 
-            "Color": [number],           ? 
-            "DevMode": [EnumDevMode],    Curent charging mode 
-            "ChgState": [EnumChargingState],  Current charging state 
-            "WifiOn": [boolean],         true if WiFi is on 
-            "AutoChg": [boolean],        true if auto-start charging is enabled
-            "ChgContinue": [boolean],    true if charging should continue after power outage 
-            "Err": [number],             current error code, 0 if there is none 
-            "Battery": [number],         EV battery capacity for EnergyManager in Wh
-            "Phases": [number],          number of phases connected 
-            "Cable": [boolean],          true if cable is connected/installed? 
-            "Auth": [boolean],           true if auth by rfid is enabled? 
-            "DsoEnabled": [boolean],     ? 
-            "EmEnabled": [boolean],      true if EnergyManager mode is enabled 
-            "MaxCurr": [number],         currently set max. charging current per phase in A
-            "MaxPwr": [number],          currently set max. charging power in W
-            "MaxCurrWb": [number]        upper limit for charging current per phase in A
-             */
-
-
+            for (let entry in buffer.data) {
+                await adapter.setStateAsync(system.Name + "." + entry, { ack: true, val: buffer.data[entry] });
+            }
         }
-
-
-
     }
     catch (e) {
         adapter.log.error("exception in read_MHCP_DevInfo [" + e + "]");
